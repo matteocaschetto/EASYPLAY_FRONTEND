6d760afc16b3d7eccb16a0bcb0c059127dd66b2b
matteocaschetto <matteocaschetto9@gmail.com>
2025-03-28 16:01:06 +0100

    COMMIT FINALE!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import logo from "../assets/EASYPLAY.svg";
+import logo from "../assets/EASYPLAY_FIXED.svg";
 import "../css/Navbar.css";
 
 const NavigationBar = () => {
@@ -12,14 +12,14 @@ const NavigationBar = () => {
           <img
             src={logo}
             alt="EasyPlay"
-            style={{ width: "180px", marginRight: "10px" }}
+            style={{ width: "150px", marginRight: "10px" }}
           />
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
+          <Nav className="ms-auto text-center">
             <Nav.Link as={Link} to="/eventi" className="text-black fs-5">
               Eventi
             </Nav.Link>
